e6c20697e1a43eb01c88b44d67bb86adb74b5156
Derick Alangi <alangiderick@gmail.com>
2021-09-17 10:52:08 +0100

    specials: Make SpecialContributions be able to view IP editors contribs
    In this patch, an issue is resolved when non AMC mode is used where by it's
    not possible to view contributions of IP editors. Inject the required services
    via the contributions special page and pass it to parent (history).
    
    NOTE - When the user is an IP, null or false is returned making the special
    contributions page to error out instead of handling the error and rendering
    a page with no contributions.
    
    Bug: T291233
    Change-Id: I8de22997772e09d38900de1ca2d374a395285293

--- includes/MobileFrontendHooks.php
+++ includes/MobileFrontendHooks.php
@@ -631,7 +631,13 @@ class MobileFrontendHooks {
 				 * Redirect people here as this is essential
 				 * Special:Contributions without the bells and whistles.
 				 */
-				$list['Contributions'] = 'SpecialMobileContributions';
+				$list['Contributions'] = [
+					"class" => "SpecialMobileContributions",
+					"services" => [
+						"NamespaceInfo",
+						"RevisionFactory"
+					]
+				];
 			}
 
 		}
