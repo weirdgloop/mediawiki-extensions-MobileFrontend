a533ae54e7c8a084d16a3582729587a7e9e607a5
Bartosz Dziewoński <matma.rex@gmail.com>
2022-09-26 20:21:50 +0200

    MobileFormatter: Remove maxImages and maxHeadings limits
    No longer needed, because the new implementation should be able to
    handle any number of images and headings, and checking the limits
    itself takes time.
    
    Bug: T318991
    Change-Id: I16b890a8409012bd7c489073891bd57bc8a3d994

--- includes/ExtMobileFrontend.php
+++ includes/ExtMobileFrontend.php
@@ -115,13 +115,6 @@ class ExtMobileFrontend {
 			&& !$shouldUseParsoid
 		);
 
-		// https://phabricator.wikimedia.org/T232690
-		if ( !MobileFormatter::canApply( $html, $config->get( 'MFMobileFormatterOptions' ) ) ) {
-			// In future we might want to prepend a message feeding
-			// back to the user that the page is not mobile friendly.
-			return $html;
-		}
-
 		$formatter = new MobileFormatter( $html );
 
 		$hookRunner = new HookRunner( $services->getHookContainer() );
